01886deec3858b4d116de5abedb36d5ae19a34b5
Andriy Sakharchuk <andreysakharchuk56@gmail.com>
2025-10-26 17:21:39 +0100

    Realizzato il bottone con START, STOP e RESET. Realizzato la divisione di tempo in ore, minuti, secondi

--- clockstream/lib/main.dart
+++ clockstream/lib/main.dart
@@ -1,15 +1,14 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-late final Stream<int> _orologioStream;
-
 Stream<int> orologio(Stream<int> oscillatore) async* {
   int seconds = 0;
   await for (final s in oscillatore) {
-    yield seconds++;
+    yield seconds += 30;
   }
 }
 
@@ -19,11 +18,6 @@ Stream<int> ticker(Duration interval) async* {
     yield 1;
   }
 }
-
-int ciao() {
-  return 1;
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -48,14 +42,56 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+enum StatoOrologio { START, STOP, RESET }
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  var stream = ticker(const Duration(seconds: 1));
+  StreamSubscription<int>? _subscription;
 
-  void initState() {
-    super.initState();
-    final tickerStream = ticker(const Duration(seconds: 1));
-    _orologioStream = orologio(tickerStream);
+  StatoOrologio _statoOrologio1 = StatoOrologio.RESET;
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+
+  void _modificaStatoOrologio1() {
+    setState(() {
+      switch (_statoOrologio1) {
+        case StatoOrologio.START:
+          _statoOrologio1 = StatoOrologio.STOP;
+          _subscription?.cancel();
+          _subscription = null;
+          break;
+        case StatoOrologio.STOP:
+          _statoOrologio1 = StatoOrologio.RESET;
+          _subscription?.cancel();
+          _subscription = null;
+          _counter = 0;
+          break;
+        case StatoOrologio.RESET:
+          _statoOrologio1 = StatoOrologio.START;
+          final stream = orologio(ticker(const Duration(seconds: 1)));
+          _subscription = stream.listen((seconds) {
+            setState(() {
+              _counter = seconds;
+            });
+          });
+          break;
+      }
+    });
+  }
+
+  Widget _iconaOrologio1() {
+    switch (_statoOrologio1) {
+      case StatoOrologio.START:
+        return const Icon(Icons.stop);
+      case StatoOrologio.STOP:
+        return const Icon(Icons.refresh);
+      case StatoOrologio.RESET:
+        return const Icon(Icons.play_arrow);
+    }
   }
 
   @override
@@ -69,25 +105,17 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            StreamBuilder<int>(
-              stream: _orologioStream,
-              initialData: 0,
-              builder: (context, snapshot) {
-                final int counter = snapshot.data ?? 0;
-                return Text(
-                  '$counter',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                );
-              },
+            const Text('Orologio:'),
+            Text('Ore: ${_counter ~/ 3600} Minuti: ${(_counter ~/ 60) % 60} Secondi: ${_counter % 60}',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: _modificaStatoOrologio1,
+        tooltip: _statoOrologio1.name,
+        child: _iconaOrologio1(),
       ),
     );
   }
