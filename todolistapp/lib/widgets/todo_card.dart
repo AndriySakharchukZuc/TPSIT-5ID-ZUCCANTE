503e1a9c970ffb786720717ed5987be66f67d9dd
Andriy Sakharchuk <andriy.sakharchuk@itiszuccante.edu.it>
2026-01-27 17:05:21 +0100

    new project zkeep

---
+++ todolistapp/lib/widgets/todo_card.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:todolistapp/widgets/todo_item.dart';
+
+class TaskCard extends StatefulWidget {
+  TaskCard({required this.TaskItem});
+  final TaskItem taskItems;
+}
